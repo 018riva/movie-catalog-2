c0bc12d36ec92638a17e9d2901fde2b65e3430ac
riva <rivich18@gmail.com>
2023-07-02 16:51:59 +0300

    table-movie

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,35 @@
-function Movies() {
-    return (
-      <>
-      <div>
-        <h1>Movies page</h1>
-      </div>
-      </>
-    )
-  }
+import React, { useEffect, useState } from 'react';
+import { JsonServer } from "./table/JsonServer";
+
+const Movies = () => {
+  const [filmsData, setFilmsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3031/movies');
+        const data = await response.json();
+
+        setFilmsData(data);
+      } catch (error) {
+        console.error('Помилка завантаження даних:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      { Header: 'ID', accessor: 'id' },
+      { Header: 'Title', accessor: 'title' },
+      { Header: 'Year', accessor: 'year' },
+      { Header: 'Actors', accessor: 'actors'}
+    ],
+    []
+  );
+
+  return <JsonServer columns={columns} data={filmsData} />;
+};;
   
-  export {Movies}
+export {Movies}
